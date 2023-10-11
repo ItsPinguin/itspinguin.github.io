3dff5afcfaf97c82773eb6c3beae2f1a519072be
ItsPinguin <pingdevyt@gmail.com>
2023-10-11 19:22:49 +0200

    Test

--- script1.js
+++ script1.js
@@ -5,7 +5,10 @@ document.getElementById("gradient").addEventListener( "mousemove", (e) => {
         mouseXpercentage = Math.round(e.pageX / windowWidth * 100);
         mouseYpercentage = Math.round(e.pageY / windowHeight * 100);
         for (const grad of document.getElementsByClassName("radial-gradient")) {
-                grad.style.background='radial-gradient(circle at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, rgb(60,60,80), rgb(40,40,60) 25%';
+                grad.style.background='radial-gradient(circle at ' + e.pageX + 'px ' + e.pageY + 'px, rgb(60,60,80), rgb(40,40,60) 20%';
+        }
+        for (const header of document.getElementsByClassName("header")) {
+                header.style.background='radial-gradient(circle at ' + (0.7*(mouseXpercentage-50)+50) + '% ' + 100 + '%, rgb(60,60,80) 1%, rgb(40,40,60) 85%, rgb(30,30,40) 100% ' + (100-(mouseYpercentage*0.8+10)) + '%';
         }
 });
-
+console.log(localStorage.getItem("key"));
